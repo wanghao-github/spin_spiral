293517ca1cc841bd6e99acc588cc985ff70cbda1
Hao Wang <wanghao90515@163.com>
2024-09-24 16:38:28 +0200

    diag

--- ahc_shc_anc_hao_with_rho.f90
+++ ahc_shc_anc_hao_with_rho.f90
@@ -401,10 +401,10 @@
          ! rho(:,:) = rho(:,:) +MATMUL(eigvecs * fermi(eigvals-efermi, Beta_fake), eigvecs_dag)*(1/knv3)
          ! rho(:,:) = rho(:,:) + MATMUL(eigvecs * fermi_array(eigvals-efermi, Beta_fake), eigvecs_dag)/knv3
          ! 确保 eigvecs 和 fermi_array 的维度匹配
-         rho(:,:) = rho(:,:) + matmul(eigvecs * fermi_array(eigvals-efermi, Beta_fake), eigvecs_dag) / knv3
+         rho(:,:) = rho(:,:) + matmul(eigvecs * diag(fermi_array(eigvals-efermi, Beta_fake)), eigvecs_dag) / knv3
 
          ! write(*,*) "knv3", knv3
-         ! write(*,*) "eigvecs= ", eigvecs 
+         write(*,*) "eigvecs= ", eigvecs 
          write(*,*) "eigvals= ", eigvals
          ! write(*,*) "eigvals-efermi", eigvals-efermi
          ! write(*,*) "efermi= ", efermi
